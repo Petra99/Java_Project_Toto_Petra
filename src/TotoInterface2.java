0c3077e8117afd661500ef04957dbb4bad358d25
DESKTOP-DOO9V8G\Petra <petrasmile.1@gmail.com>
2017-01-26 14:32:52 +0200

    Rewrite logic. Added the number of guessed numbers. Added prize.

--- src/TotoInterface2.java
+++ src/TotoInterface2.java
@@ -15,29 +15,31 @@ public class TotoInterface2 {
 
 	private JFrame frame;
 	private JButton btnStart;
-	private static JTextField textField_1;
-	private static JTextField textField_2;
-	private static JTextField textField_3;
-	private JLabel label_4;
+	private static JTextField txtFieldUserInput;
 	private JLabel label_5;
 	private JLabel label_6;
 	private JLabel label_1;
-	private JLabel lblWinningNums;
-	private JLabel label_2;
+	private JLabel lblWinningNums_1;
+	private JLabel lblIii_1;
 	private JLabel label_equals_3;
 	private JLabel lblNewLabel_2;
-	private JLabel lblNewLabel_3;
-	private JLabel lblNewLabel_4;
-	static int[] arrRand;
+	static int[] arrRand_1;
+	static int[] arrRand_2;
+	static int[] arrRand_3;
 	static int arrayLenght = 6;
 	private JLabel label_equals_2;
 	private JLabel label_equals_1;
+	private JLabel lblWinningNums_2;
+	private JLabel lblWinningNums_3;
+	private JLabel label_4;
+	private JLabel lblPrize_1;
+	private JLabel lblPrize_2;
+	private JLabel lblPrize_3;
+
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		arrRand = new int[arrayLenght];
-		getArrayWithRandomNums(arrRand);
 
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -49,6 +51,17 @@ public class TotoInterface2 {
 				}
 			}
 		});
+	}
+
+	private static void generateRandomNumsArrs() {
+		arrRand_1 = new int[arrayLenght];
+		getArrayWithRandomNums(arrRand_1);
+
+		arrRand_2 = new int[arrayLenght];
+		getArrayWithRandomNums(arrRand_2);
+
+		arrRand_3 = new int[arrayLenght];
+		getArrayWithRandomNums(arrRand_3);
 	}
 
 	/**
@@ -75,135 +88,131 @@ public class TotoInterface2 {
 		JLabel lblI = new JLabel("I \u043A\u0440\u044A\u0433 :");
 		lblI.setHorizontalAlignment(SwingConstants.CENTER);
 		lblI.setVerticalAlignment(SwingConstants.TOP);
-		lblI.setBounds(182, 100, 70, 14);
+		lblI.setBounds(65, 202, 70, 14);
 		frame.getContentPane().add(lblI);
 
 		JLabel lblIi = new JLabel("II \u043A\u0440\u044A\u0433 :");
 		lblIi.setHorizontalAlignment(SwingConstants.CENTER);
-		lblIi.setBounds(182, 200, 70, 14);
+		lblIi.setBounds(65, 258, 70, 14);
 		frame.getContentPane().add(lblIi);
 
 		JLabel lblIii = new JLabel("III \u043A\u0440\u044A\u0433 :");
 		lblIii.setHorizontalAlignment(SwingConstants.CENTER);
-		lblIii.setBounds(182, 300, 70, 14);
+		lblIii.setBounds(65, 314, 70, 14);
 		frame.getContentPane().add(lblIii);
 
 		btnStart = new JButton("start");
 		btnStart.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-			
-			String array1 = textField_1.getText();
-			String[] splittedStr1 = array1.split(" ");
-			int[] userArrWithInts_1 = stringToInt(splittedStr1);
-			int br1 = compare(userArrWithInts_1,arrRand);
-			
-			String array2 = textField_2.getText();
-			String[] splittedStr2 = array2.split(" ");
-			int[] userArrWithInts_2 = stringToInt(splittedStr2);
-			int br2 = compare(userArrWithInts_2,arrRand);
-			
-			String array3 = textField_3.getText();
-			String[] splittedStr3 = array3.split(" ");
-			int[] userArrWithInts_3 = stringToInt(splittedStr3);
-			int br3 = compare(userArrWithInts_3,arrRand);
-			
-			lblWinningNums.setText(intArrToString(arrRand));
-			
-			label_equals_1.setText(String.valueOf(br1));
-			
-			label_equals_2.setText(String.valueOf(br2));
-			
-			label_equals_3.setText(String.valueOf(br3));
-			
-			//printPrize();
-			}
+				generateRandomNumsArrs();
 
+				String array1 = txtFieldUserInput.getText();
+				String[] splittedStr1 = array1.split(" ");
+				int[] userArrWithInts_1 = stringToInt(splittedStr1);
+
+				int br1 = compare(userArrWithInts_1, arrRand_1);
+				int br2 = compare(userArrWithInts_1, arrRand_2);
+				int br3 = compare(userArrWithInts_1, arrRand_3);
+
+				lblWinningNums_1.setText(intArrToString(arrRand_1));
+				lblWinningNums_2.setText(intArrToString(arrRand_2));
+				lblWinningNums_3.setText(intArrToString(arrRand_3));
+
+				label_equals_1.setText(String.valueOf(br1));
+				label_equals_2.setText(String.valueOf(br2));
+				label_equals_3.setText(String.valueOf(br3));
+
+				lblPrize_1.setText(String.valueOf(getPrize(br1)) + "ыт");
+				lblPrize_2.setText(String.valueOf(getPrize(br2)) + "ыт");
+				lblPrize_3.setText(String.valueOf(getPrize(br3)) + "ыт");
+			}
 		});
+		
 		btnStart.setBounds(172, 400, 89, 23);
 		frame.getContentPane().add(btnStart);
 
-		textField_1 = new JTextField("12 22 31 23 34 26");
+		txtFieldUserInput = new JTextField();
 
-		textField_1.setBounds(65, 130, 300, 20);
+		txtFieldUserInput.setBounds(65, 130, 300, 20);
 
-		frame.getContentPane().add(textField_1);
-		textField_1.setColumns(10);
+		frame.getContentPane().add(txtFieldUserInput);
+		txtFieldUserInput.setColumns(10);
 
-		textField_2 = new JTextField("12 22 31 23 34 26");
-		textField_2.setColumns(10);
-		textField_2.setBounds(65, 230, 300, 20);
-		frame.getContentPane().add(textField_2);
-
-		textField_3 = new JTextField("12 22 31 23 34 26");
-		textField_3.setColumns(10);
-		textField_3.setBounds(65, 330, 300, 20);
-		frame.getContentPane().add(textField_3);
-		
-		label_4 = new JLabel();
-		label_4.setBounds(304, 90, 46, 14);
-		frame.getContentPane().add(label_4);
-		
-		label_6 = new JLabel("\u041C\u043E\u043B\u044F, \u0432\u044A\u0432\u0435\u0434\u0435\u0442\u0435 \u0432\u0430\u0448\u0438\u0442\u0435 6 \u043F\u0435\u0447\u0435\u043B\u0438\u0432\u0448\u0438 \u0447\u0438\u0441\u043B\u0430 \u0437\u0430 \u0432\u0441\u0435\u043A\u0438 \u043E\u0442 \u0442\u0440\u0438\u0442\u0435 \u043A\u0440\u044A\u0433\u0430");
+		label_6 = new JLabel(
+				"\u041C\u043E\u043B\u044F, \u0432\u044A\u0432\u0435\u0434\u0435\u0442\u0435 \u0432\u0430\u0448\u0438\u0442\u0435 6 \u043F\u0435\u0447\u0435\u043B\u0438\u0432\u0448\u0438 \u0447\u0438\u0441\u043B\u0430");
 		label_6.setHorizontalAlignment(SwingConstants.CENTER);
 		label_6.setBounds(24, 78, 400, 14);
 		frame.getContentPane().add(label_6);
-		
+
 		label_1 = new JLabel("\u041F\u0435\u0447\u0435\u043B\u0438\u0432\u0448\u0438 \u0447\u0438\u0441\u043B\u0430 :");
 		label_1.setHorizontalAlignment(SwingConstants.CENTER);
-		label_1.setBounds(142, 430, 150, 14);
+		label_1.setBounds(117, 176, 150, 14);
 		frame.getContentPane().add(label_1);
-		
-		lblWinningNums = new JLabel();
-		lblWinningNums.setHorizontalAlignment(SwingConstants.CENTER);
-		lblWinningNums.setBounds(117, 460, 200, 14);
-		frame.getContentPane().add(lblWinningNums);
-		
-		label_2 = new JLabel("\u0411\u0440\u043E\u0439 \u0441\u044A\u0432\u043F\u0430\u0434\u0435\u043D\u0438\u044F");
-		label_2.setBounds(10, 536, 100, 14);
-		frame.getContentPane().add(label_2);
-		
+
+		lblIii_1 = new JLabel("III \u0442\u0435\u0433\u043B\u0435\u043D\u0435");
+		lblIii_1.setBounds(10, 536, 100, 14);
+		frame.getContentPane().add(lblIii_1);
+
 		lblNewLabel_2 = new JLabel("\u041F\u0435\u0447\u0430\u043B\u0431\u0430:");
-		lblNewLabel_2.setBounds(215, 536, 50, 14);
+		lblNewLabel_2.setBounds(191, 455, 50, 14);
 		frame.getContentPane().add(lblNewLabel_2);
-		
-		lblNewLabel_3 = new JLabel("1 000 000");
-		lblNewLabel_3.setBounds(275, 536, 60, 14);
-		frame.getContentPane().add(lblNewLabel_3);
-		
-		lblNewLabel_4 = new JLabel("\u043B\u0432");
-		lblNewLabel_4.setBounds(345, 536, 20, 14);
-		frame.getContentPane().add(lblNewLabel_4);
-		
-		JLabel label_3 = new JLabel("\u0411\u0440\u043E\u0439 \u0441\u044A\u0432\u043F\u0430\u0434\u0435\u043D\u0438\u044F");
-		label_3.setBounds(10, 511, 100, 14);
-		frame.getContentPane().add(label_3);
-		
-		JLabel label_7 = new JLabel("\u0411\u0440\u043E\u0439 \u0441\u044A\u0432\u043F\u0430\u0434\u0435\u043D\u0438\u044F");
-		label_7.setBounds(10, 486, 100, 14);
-		frame.getContentPane().add(label_7);
-		
+
+		JLabel lblIi_1 = new JLabel("II \u0442\u0435\u0433\u043B\u0435\u043D\u0435");
+		lblIi_1.setBounds(10, 511, 100, 14);
+		frame.getContentPane().add(lblIi_1);
+
+		JLabel lblI_1 = new JLabel("I \u0442\u0435\u0433\u043B\u0435\u043D\u0435");
+		lblI_1.setBounds(10, 486, 100, 14);
+		frame.getContentPane().add(lblI_1);
+
 		label_equals_1 = new JLabel("-");
 		label_equals_1.setBounds(117, 485, 20, 14);
 		frame.getContentPane().add(label_equals_1);
-		
+
 		label_equals_2 = new JLabel("-");
 		label_equals_2.setBounds(117, 511, 20, 14);
 		frame.getContentPane().add(label_equals_2);
-		
+
 		label_equals_3 = new JLabel("-");
 		label_equals_3.setBounds(117, 536, 20, 14);
 		frame.getContentPane().add(label_equals_3);
-		
-		
-		
-	
+
+		lblWinningNums_1 = new JLabel("-");
+		lblWinningNums_1.setHorizontalAlignment(SwingConstants.LEFT);
+		lblWinningNums_1.setBounds(165, 202, 200, 14);
+		frame.getContentPane().add(lblWinningNums_1);
+
+		lblWinningNums_2 = new JLabel("-");
+		lblWinningNums_2.setBounds(165, 258, 200, 14);
+		frame.getContentPane().add(lblWinningNums_2);
+
+		lblWinningNums_3 = new JLabel("-");
+		lblWinningNums_3.setBounds(165, 314, 200, 14);
+		frame.getContentPane().add(lblWinningNums_3);
+
+		label_4 = new JLabel("\u0411\u0440\u043E\u0439 \u0441\u044A\u0432\u043F\u0430\u0434\u0435\u043D\u0438\u044F");
+		label_4.setBounds(10, 455, 100, 14);
+		frame.getContentPane().add(label_4);
+
+		lblPrize_1 = new JLabel("-");
+		lblPrize_1.setBounds(191, 486, 150, 14);
+		frame.getContentPane().add(lblPrize_1);
+
+		lblPrize_2 = new JLabel("-");
+		lblPrize_2.setBounds(191, 511, 150, 14);
+		frame.getContentPane().add(lblPrize_2);
+
+		lblPrize_3 = new JLabel("-");
+		lblPrize_3.setBounds(191, 536, 150, 14);
+		frame.getContentPane().add(lblPrize_3);
+
 	}
 
 	private String intArrToString(int[] arrRand2) {
 		int arrayL = 6;
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < arrayL; i++) {
-		        sb.append(String.valueOf(arrRand2[i]) + " ");
+			sb.append(String.valueOf(arrRand2[i]) + " ");
 
 		}
 		return sb.toString();
@@ -223,20 +232,21 @@ public class TotoInterface2 {
 			}
 		}
 	}
-	 
-	private static int[] stringToInt(String[] userString ){
+
+	private static int[] stringToInt(String[] userString) {
 		int[] userArrayWithInts = new int[arrayLenght];
 
 		for (int i = 0; i < arrayLenght; i++) {
-		   // try {
-		        userArrayWithInts[i] = Integer.parseInt(userString[i]);
-		   // } catch (NumberFormatException nfe) {
-		        
-		   // };
+			// try {
+			userArrayWithInts[i] = Integer.parseInt(userString[i]);
+			// } catch (NumberFormatException nfe) {
+
+			// };
 		}
 		return userArrayWithInts;
 
-}
+	}
+
 	private static int compare(int[] userArray, int[] randArray) {
 		int coincidence = 0;
 		for (int userNumsIndex = 0; userNumsIndex < userArray.length; userNumsIndex++) {
@@ -246,22 +256,28 @@ public class TotoInterface2 {
 			}
 		}
 		return coincidence;
-		
-		
-	}
-	
 
-	private static double printPrize(int guessedNums) {
+	}
+
+	private static double getPrize(int guessedNums) {
 		double prize = 0;
-		switch(guessedNums){
-		case 3: prize = 100;break;
-		case 4: prize = 700;break;
-		case 5: prize = 1500;break;
-		case 6: prize = 1000000;break;
-		default: prize =0.00;
+		switch (guessedNums) {
+		case 3:
+			prize = 3.30;
+			break;
+		case 4:
+			prize = 50.10;
+			break;
+		case 5:
+			prize = 1500.00;
+			break;
+		case 6:
+			prize = 1000000.00;
+			break;
+		default:
+			prize = 0.00;
 		}
 		return prize;
-		
-		
+
 	}
 }
